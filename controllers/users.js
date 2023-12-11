33de21128c82eb05987e5f53b758f2b6c9de4048
smartful <remi_rodrigues@hotmail.com>
2023-12-12 00:17:45 +0100

    user register endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,38 @@
-import { response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
-export const register = (request, response) => {
+export const register = async (request, response) => {
   const { name, email, password } = request.body;
-  console.log('name : ', name);
-  console.log('email : ', email);
-  console.log('password : ', password);
 
-  response.json({ name, email });
+  try {
+    // Check if user already exist
+    let user = await User.findOne({ email });
+    if (user) {
+      return response.status(400).json({ message: 'User already exist' });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    // Generate token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (error, token) => {
+      if (error) throw error;
+      return response.json({ token });
+    });
+  } catch (error) {
+    console.error(`Error : ${error.message}`);
+    return response.status(500).json({ message: 'Server Error' });
+  }
 };
