3324b0cfec7cd3b31d72384348c12ae0bd9e6718
smartful <remi_rodrigues@hotmail.com>
2024-01-01 03:12:06 +0100

    identify environment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.CLIENT_URL,
   })
 );
 app.use(express.json());
