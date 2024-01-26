6cb3dac14555022ad941b1a8f975915d0ea70785
smartful <remi_rodrigues@hotmail.com>
2024-01-26 04:03:42 +0100

    stripe portal and webhook wip

--- controllers/subscribtions.js
+++ controllers/subscribtions.js
@@ -75,10 +75,94 @@ export const createCheckoutSession = async (request, response) => {
     mode: 'subscription',
     success_url: `${YOUR_DOMAIN}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    subscription_data: {
+      trial_period_days: 7,
+    },
   });
   console.log('session : ', session);
 
   response.json({ url: session?.url });
+};
+
+export const createPortalSession = async (request, response) => {
+  // For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
+  // Typically this is stored alongside the authenticated user in your database.
+  // const { session_id } = request.body;
+  // const session_id = 'cs_test_a1H51P2Mxjx825QRYc2s925UUNl0WOWRRUMQFRmbzJ3m0QpuwAOzGI6l2l';
+  // const checkoutSession = await stripe.checkout.sessions.retrieve(session_id);
+
+  // This is the url to which the customer will be redirected when they are done
+  // managing their billing with the portal.
+  const returnUrl = YOUR_DOMAIN;
+
+  // const user = await User.findById(request.user.id).select('-password');
+
+  // if (!user) {
+  //   return response.status(400).json({ message: 'User not found' });
+  // }
+  // customer = await stripe.customers.retrieve(user?.stripeCustomerId);
+  const customer = await stripe.customers.retrieve('cus_PRYYVD0ifln5CE');
+  console.log('customer : ', customer);
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: customer,
+    return_url: returnUrl,
+  });
+
+  console.log('portalSession : ', portalSession);
+
+  response.json({ url: portalSession?.url });
+};
+
+export const subscribtionWebhook = async (request, response) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  if (webhookSecret) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = request.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, signature, webhookSecret);
+    } catch (error) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return response.status(400).json({ message: error.message });
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = request.body.data;
+    eventType = request.body.type;
+  }
+
+  switch (eventType) {
+    case 'checkout.session.completed':
+      // Payment is successful and the subscription is created.
+      // You should provision the subscription and save the customer ID to your database.
+      console.log('WEBHOOK : checkout.session.completed');
+      break;
+    case 'invoice.paid':
+      // Continue to provision the subscription as payments continue to be made.
+      // Store the status in your database and check when a user accesses your service.
+      // This approach helps you avoid hitting rate limits.
+      console.log('WEBHOOK : invoice.paid');
+      break;
+    case 'invoice.payment_failed':
+      // The payment failed or the customer does not have a valid payment method.
+      // The subscription becomes past_due. Notify your customer and send them to the
+      // customer portal to update their payment information.
+      console.log('WEBHOOK : invoice.payment_failed');
+      break;
+    default:
+    // Unhandled event type
+  }
+
+  response.status(200).json({ message: eventType });
 };
 
 export const deleteSubscribtion = async (request, response) => {};
--- routes/subscribtions.js
+++ routes/subscribtions.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import { protect } from '../middlewares/auth.js';
-import { createCheckoutSession, createSubscribtion, deleteSubscribtion } from '../controllers/subscribtions.js';
+import {
+  createCheckoutSession,
+  createPortalSession,
+  createSubscribtion,
+  subscribtionWebhook,
+  deleteSubscribtion,
+} from '../controllers/subscribtions.js';
 
 const router = express.Router();
 
 router.post('/', protect, createSubscribtion);
 router.post('/create-checkout-session', createCheckoutSession);
+router.post('/create-portal-session', protect, createPortalSession);
+router.post('/webhook', subscribtionWebhook);
 router.delete('/:id', protect, deleteSubscribtion);
 
 export default router;
